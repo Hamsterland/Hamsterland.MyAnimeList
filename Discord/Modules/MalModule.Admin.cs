172be61781a311d7986fc16794737dafa4fd52fb
mdabr <matthewtrip2@gmail.com>
2021-07-17 13:46:30 +0100

    Allow owner and moderators to use unlink command

--- Discord/Modules/MalModule.Admin.cs
+++ Discord/Modules/MalModule.Admin.cs
@@ -8,11 +8,21 @@ namespace Hamsterland.MyAnimeList.Modules
     {
         [Command("unlink", RunMode = RunMode.Async)]
         [Summary("Unlinks a user's Discord account from their MyAnimeList account.")]
-        [RequireUserPermission(GuildPermission.BanMembers)]
+        [RequireOwner]
+        [Priority(10)]
         public async Task Unlink(IGuildUser user)
         {
             await _accountService.Delete(user.Id);
             await ReplyAsync($"Unliked {user}'s Discord account from their MyAnimeList account.");
+        }
+        
+        [Command("unlink", RunMode = RunMode.Async)]
+        [Summary("Unlinks a user's Discord account from their MyAnimeList account.")]
+        [RequireUserPermission(GuildPermission.BanMembers)]
+        [Priority(5)]
+        public async Task UnlinkModerator(IGuildUser user)
+        {
+            await Unlink(user);
         }
     }
 }
