7e28312d56ec5069ef2f93001b2dce3e8ba5c7cd
Abrar Hassan <abrarhassan.pro@gmail.com>
2021-11-02 18:59:19 +0000

    Fixes I think

--- Discord/Modules/MalModule.Admin.cs
+++ Discord/Modules/MalModule.Admin.cs
@@ -1,16 +1,43 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 
 namespace Hamsterland.MyAnimeList.Modules
 {
+    public class RequireBotOwnerOrPermissionAttribute : PreconditionAttribute
+    {
+        public RequireBotOwnerOrPermissionAttribute(GuildPermission guildPermission)
+        {
+            GuildPermission = guildPermission;
+        }
+
+        public GuildPermission GuildPermission { get; }
+        
+        public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+        {
+            var user = context.User as IGuildUser;
+
+            if (user.Id == 330746772378877954)
+            {
+                return Task.FromResult(PreconditionResult.FromSuccess());
+            }
+
+            if (user.GuildPermissions.Has(GuildPermission))
+            {
+                return Task.FromResult(PreconditionResult.FromSuccess());
+            }
+
+            return Task.FromResult(PreconditionResult.FromError("No!"));
+        }
+    }
+
     public partial class MalModule
     {
         [Command("verify")]
         [Summary("Gives the verified role to a user.")]
-        [RequireUserPermission(GuildPermission.BanMembers)]
-        [Priority(10)]
+        [RequireBotOwnerOrPermission(GuildPermission.BanMembers)]
         public async Task Verify(IGuildUser user)
         {
             if (user.RoleIds.Contains(_verifiedRoleId))
@@ -22,40 +49,14 @@ namespace Hamsterland.MyAnimeList.Modules
             await user.AddRoleAsync(_verifiedRoleId);
             await ReplyAsync($"Given the verified role to this {user}");
         }
-        
-        [Command("verify")]
-        [Summary("Gives the verified role to a user.")]
-        [RequireOwner]
-        [Priority(5)]
-        public async Task VerifyModerator(IGuildUser user)
-        {
-            if (user.RoleIds.Contains(_verifiedRoleId))
-            {
-                await ReplyAsync("This user already has the verified role.");
-                return;
-            }
 
-            await user.AddRoleAsync(_verifiedRoleId);
-            await ReplyAsync($"Given the verified role to this {user}");
-        }
-        
         [Command("unlink", RunMode = RunMode.Async)]
         [Summary("Unlinks a user's Discord account from their MyAnimeList account.")]
-        [RequireOwner]
-        [Priority(10)]
+        [RequireBotOwnerOrPermission(GuildPermission.BanMembers)]
         public async Task Unlink(IUser user)
         {
             await _accountService.Delete(user.Id);
             await ReplyAsync($"Unliked {user}'s Discord account from their MyAnimeList account.");
-        }
-        
-        [Command("unlink", RunMode = RunMode.Async)]
-        [Summary("Unlinks a user's Discord account from their MyAnimeList account.")]
-        [RequireUserPermission(GuildPermission.BanMembers)]
-        [Priority(5)]
-        public async Task UnlinkModerator(IUser user)
-        {
-            await Unlink(user);
         }
     }
 }
